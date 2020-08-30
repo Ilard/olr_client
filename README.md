c99b80b2616f996bc9475be4e6600abfb489f648
Rémy Mondi <remy@mondi.dev>
2020-08-30 16:00:15 +0200

    Add error if empty login

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This project is still in progress...
 
 ## Installation for production
-* Copy and rename **default-env** to **.env** `mv defauly-env .env`;
+* Copy and rename **default-env** to **.env** `mv default-env .env`;
 * Set **REACT_APP_API_URL** with the URL of the API
--- src/Login.js
+++ src/Login.js
@@ -4,26 +4,27 @@ import './Login.css';
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {username:''};
+    this.state = {
+      error:false,
+      username:''
+    };
   }
   editUsername = (event) => {
     this.setState({username:event.target.value});
   }
-  handlerKeyPress = (event) => {
-    if (event.charCode === 13) {
-      this.props.app.setUsername(event.target.value);
+  updateUsername = () => {
+    const username = this.state.username;
+    if (username !== '') {
+      this.props.app.setUsername(username);
+    } else {
+      this.setState({error:true});
     }
-  }
-  handlerClick = (event) => {
-    this.props.app.setUsername(event.target.value);
   }
   render() {
     return (
       <div className='login-container'>
-        <form className='login-form'>
-          <input className='login-field' type='text' onChange={this.editUsername} onKeyPress={this.handlerKeyPress} value={this.state.username} />
-          <input className='button' type='submit' onClick={this.handlerClick} value='JOUER !'/>
-        </form>
+        <input className={this.state.error ? 'login-field login-field-error' : 'login-field'} type='text' onChange={this.editUsername} placeholder='Pseudo' value={this.state.username} />
+        <button className='button' onClick={this.updateUsername}>Jouer</button>
       </div>
     );
   }
