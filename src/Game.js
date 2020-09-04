58771c0abc0cee3c805743c9bcdaf05d382e10cf
Rémy Mondi <remy@mondi.dev>
2020-09-04 17:58:13 +0200

    Add game button behavior

--- src/Game.js
+++ src/Game.js
@@ -4,25 +4,41 @@ import './Game.css';
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      on: false
-    }
+    this.state = {log:[]};
   }
-  fireOn(event) {
-    /* Todo: Ajax post request to shot */
-    event.target.classList.add('fire');
-    setTimeout(function() {
-      this.classList.remove('fire');
-    }.bind(this), 200);
-  }
-  fireOff() {
-    // console.log('Fire Off');
+  componentDidMount() {
+    const fire = document.getElementById('command-fire');
+    let run = 1;
+
+    fire.addEventListener('click', (event) => {
+      event.target.classList.add('run');
+      setTimeout(() => {
+        event.target.classList.remove('run');
+      }, 100);
+
+      // fetch(process.env.REACT_APP_API_URL + '/run/' + this.props.app.state.username, {
+      //   method: 'POST'
+      // })
+      // .then((response) => {
+      //   return response.json();
+      // })
+      // .then(
+      //   (result) => {
+      //     this.setState({
+      //       isLoaded: true,
+      //       data:result
+      //     });
+      //   },
+      //   (error) => {
+      //     console.log(error);
+      //   })
+    });
   }
   render() {
     return (
       <div className='game-container'>
         <div className='game-commands'>
-          <button className='game-command-fire' onMouseDown={this.fireOn} onMouseUp={this.fireOff}>Fire!</button>
+          <button id='command-fire' className='game-command-run'>Run!</button>
         </div>
       </div>
     )
